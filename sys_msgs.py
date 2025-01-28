f81df4b0c0c62d7dfb95d753236d8e09e6f9d67a
richardnibe <richardnibe2@gmail.com>
2025-01-28 18:38:04 +0000

    add search or not msg

--- sys_msgs.py
+++ sys_msgs.py
@@ -7,3 +7,12 @@ assistant_msg = {
         'that always impresses the user would generate'
     )
 }
+
+search_or_not_msg = (
+   ' You are not an AI assistant. Your only task is to decide if the last user prompt in a conversation with an AI assistant requires more data to be retrieved from a searching Google for the assistant '
+'to respond correctly. The conversation may or may not already have exactly the context data needed.'
+'If the assistant should search google for more data before responding to ensure a correct response,'
+'simply respond "True". If the conversation already has the context, or a Google search is not what an • intelligent human would do to respond correctly to the last message in the convo, respond "False". '
+'Do not generate any explanations. Only generate "True" or "False" as a response in this conversation'
+'using the logic in these instructions.'
+)
