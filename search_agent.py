1594f22256f11cc73366a9cdf70e248465227a91
RichardNibe <RICHARDNIBE2@GMAIL.COM>
2025-01-28 12:18:09 -0500

    add search_agent.py for streaming assistant conversation

---
+++ search_agent.py
@@ -0,0 +1,27 @@
+import ollama
+
+assistant_convo = []
+
+def stream_assistant_convo():
+    global assistant_convo
+    response_stream = ollama.chat(model='deepseek-r1:8b', messages=assistant_convo, stream=True)
+    complete_response =  ''
+    print('Assistant: ')
+
+    for chunk in response_stream:
+        print(chunk['message']['content'], end='', flush=True)
+        complete_response += chunk['message']['content']
+
+    assistant_convo.append({'role': 'user', 'content': complete_response})
+    print('\n\n')
+
+def main():
+    global assistant_convo
+
+    while True:
+        prompt = input('User: \n')
+        assistant_convo.append({'role': 'assistant', 'content': prompt})
+        stream_assistant_convo()
+
+if __name__ == '__main__':
+    main()
